64b326aa5121fd5bfa73f22e33521e7aff45de6f
Jean-Frédéric Fontaine <jefond@l4714-22.info.polymtl.ca>
2017-09-13 11:59:07 -0400

    changement2

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ $(document).ready(function(){
 
 
     
-    $("p.special").click(caca);
+    $("p.special").click(loadDoc("FactSage.com - Reaction module.php"));
 
-    
+    function loadDoc(para1) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("corps").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", para1, true);
+  xhttp.send();
+}
     
 });
 
-    function caca()
-{
-        $(this).fadeOut();
-}
