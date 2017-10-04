b4b78aee496e8837ea64d58ba209c33381671ff8
Samuel Ferron <saferc@l4714-26.info.polymtl.ca>
2017-10-04 17:24:27 -0400

    salut

---
+++ TD2/javascript.js
@@ -0,0 +1,33 @@
+
+//main?
+$(document).ready(function(){
+    $('#maTable').DataTable();
+    
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+});
