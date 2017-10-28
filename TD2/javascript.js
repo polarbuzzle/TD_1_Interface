b69041b2a39c4249cccb0a316ce23bbf659f9ebb
Adrimeov <jffontaine_@hotmail.com>
2017-10-27 22:26:28 -0400

    salut

--- TD2/javascript.js
+++ TD2/javascript.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
     let listeDeNom = [];
     let infoStation = [];
     let monDataTable;
+    let lattittude = 45.5016889; 
+    let longitude = -73.5672;
     
         $.getJSON("http://secure.bixi.com/data/stations.json", function(data){
         
@@ -47,7 +49,7 @@ $(document).ready(function(){
         .done(function(){
             
             map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 45.5016889, lng: -73.56725599999999},
+                center: {lat: lattittude, lng: longitude},
                 zoom: 13
             });
             
@@ -61,11 +63,35 @@ $(document).ready(function(){
     $("#tags").autocomplete({
         select: function(event, ui){
             
-            var station = ui.item.value;
+            var station = infoStation[ui.item.value];
             
-            document.getElementById("choixLocalisation").innerHTML = station;
             
-            document.getElementById("IdStation").innerHTML = infoStation[station].id
+            document.getElementById("choixLocalisation").innerHTML = station.nom;
+            
+            document.getElementById("IdStation").innerHTML = station.id;
+            
+            if(station.bloquee)
+            {
+                document.getElementById("Bloquee").innerHTML = "Oui";
+                document.getElementById("Bloquee").style.backgroundColor = "#FF5240"; 
+            }
+            else
+            {
+                 document.getElementById("Bloquee").innerHTML = "Non";
+                 document.getElementById("Bloquee").style.backgroundColor = "#04C078"; 
+            }
+            
+            if(station.suspendue)
+            {
+                document.getElementById("Suspendue").innerHTML = "Oui";
+                document.getElementById("Suspendue").style.backgroundColor = "#FF5240"; 
+            }
+            else
+            {
+                 document.getElementById("Suspendue").innerHTML = "Non";
+                 document.getElementById("Suspendue").style.backgroundColor = "#04C078"; 
+            }
+                
             
         }
     });
