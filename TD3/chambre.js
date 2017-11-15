ef5c2b0f4087224c72812bb77c403081ac9d3f17
Samuel Ferron <saferc@l4714-26.info.polymtl.ca>
2017-11-15 18:30:45 -0500

    Pas mal fini

--- TD3/chambre.js
+++ TD3/chambre.js
@@ -1,42 +1,47 @@
 /* Simule les variations de températures dans une pièce
  * @auteur: francois.lemieux@polymtl.ca 2010-07-26
 */
-var facteurChauffage      = 0.01;  // échange calorifique avec le chauffage
-var facteurIsolation      = 0.01;  // échange calorifique avec l'extérieur
-var temperatureChauffage  = 70;    // température des calorifères
-var temperatureExterieure = 0;     // température extérieure
-var temperatureInterieure = 10;    // température intérieure par défaut
-var chauffage             = false; // le chauffage n'est pas actif par défaut
-var temperatureThermostat = 20;       // le chauffage démarre à  moins de 20° par défaut
+class Chambre {
+    constructor(){
+        this.facteurChauffage      = 0.01;  // échange calorifique avec le chauffage
+        this.facteurIsolation      = 0.01;  // échange calorifique avec l'extérieur
+        this.temperatureChauffage  = 70;    // température des calorifères
+        this.temperatureExterieure = -50;     // température extérieure
+        this.temperatureInterieure = 10;    // température intérieure par défaut
+        this.chauffage             = false; // le chauffage n'est pas actif par défaut
+        this.temperatureThermostat = 20;       // le chauffage démarre à  moins de 20° par défaut
 
-var thermometreMax        =50      // Température maximale affichée par le thermomètre
-var thermometreMin        =-50     // Température minimale affichée par le thermomètre
-var intervalleTemps       =1000;   // intervalle en milisecondes de lecture de la température
-var tailleThermometre     =300;    // Taille du thermomètre en pixels
-var positionThermometre   =50;     // Position du thermomètre par rapport au haut de la page
-
-/*Extrait la température réglée par le thermostatr */
-function getTemperatureThermostat(){
-  return document.getElementById("tdValeurThermostat").innerHTML;
-}
-
-/* Définit les échanges calorifiques selon les valeurs
- * de la temppérature extérieure, de l'isolation,
- * de la température intérieure, de la température fixée
- * par le thermostat et par l'efficacité du système
- * de chauffage.
- * Un "ticTac" correspond à  un échange calorifique
- */
-function ticTac() {
-  temperatureInterieure += ((temperatureExterieure - temperatureInterieure) * facteurIsolation);
-
-  if (chauffage == true) {
-    temperatureInterieure += ((temperatureChauffage - temperatureInterieure) * facteurChauffage);
-
-    if (temperatureInterieure > getTemperatureThermostat()) {
-      chauffage=false;
+        this.thermometreMax        =50      // Température maximale affichée par le thermomètre
+        this.thermometreMin        =-50     // Température minimale affichée par le thermomètre
+        this.intervalleTemps       =1000;   // intervalle en milisecondes de lecture de la température
+        this.tailleThermometre     =300;    // Taille du thermomètre en pixels
+        this.positionThermometre   =50;     // Position du thermomètre par rapport au haut de la page
     }
-  } else if (temperatureInterieure < getTemperatureThermostat()) {
-    chauffage=true;
-  }
+
+    /*Extrait la température réglée par le thermostatr */
+    getTemperatureThermostat(){
+    return document.getElementById("tdValeurThermostat").innerHTML;
+    }
+
+    /* Définit les échanges calorifiques selon les valeurs
+    * de la temppérature extérieure, de l'isolation,
+    * de la température intérieure, de la température fixée
+    * par le thermostat et par l'efficacité du système
+    * de chauffage.
+    * Un "ticTac" correspond à  un échange calorifique
+    */
+    ticTac() {
+        this.temperatureInterieure += ((this.temperatureExterieure - this.temperatureInterieure) * this.facteurIsolation);
+
+        if (this.chauffage == true) {
+            this.temperatureInterieure += ((this.temperatureChauffage - this.temperatureInterieure) * this.facteurChauffage);
+
+            if (this.temperatureInterieure > this.getTemperatureThermostat()) {
+            this.chauffage=false;
+            }
+        } else if (this.temperatureInterieure < this.getTemperatureThermostat()) {
+            this.chauffage=true;
+        }
+        return {};
+    }
 }
--- TD3/thermostat.js
+++ TD3/thermostat.js
@@ -1,13 +1,88 @@
 /* Extrait les valeurs produites dans la page Web et par le simulateur
  * et déclanche l'affichage des valeurs dans la page
 */
+
   var var1 = 0;
+
+
+  
+    
+  
+    /**/
+class observerThermo {
+    constructor() {
+        //this.element = document.getElementById(idHTML);
+    }
+    update(chambre) {
+        var temp = chambre.temperatureInterieure;
+        if (temp >= -50 || temp <= 50)
+            document.getElementById("barreThermo").style.height = (50 + temp).toString() + "%";
+        else if(temp > 50)
+            document.getElementById("barreThermo").style.height = "100%";
+        else if(temp < -50)
+            document.getElementById("barreThermo").style.height = "0%";
+            
+        /*AJOUTER LE CHIFFRE*/
+    }
+}
+
+class observerInfo{
+    constructor() {
+        //on fait rien?
+    }
+    
+    update(chambre) {
+        
+        if(chambre.chauffage) {
+            document.getElementById("etatChauffage").innerHTML = "ACTIF";
+            document.getElementById("etatChauffage").style.background = "red";
+        }
+        else {
+            document.getElementById("etatChauffage").innerHTML = "INACTIF";
+            document.getElementById("etatChauffage").style.background = "white";
+        }
+        document.getElementById("tempExt").innerHTML = chambre.temperatureExterieure;
+        document.getElementById("thermostat").innerHTML = chambre.temperatureThermostat;
+        
+    }
+    
+}
+  
+  /*Observable : chambre*/
+class ObservableChambre {
+        constructor(chambre){
+            this.chambre = chambre;
+            this.observers = [];
+        }
+        
+        notifyAll(){
+            chambre.ticTac();
+            this.observers.forEach(function(element){
+                element.update(chambre);
+            });
+        }
+        
+        addObserver(newObserver) {
+            this.observers.push(newObserver);
+        }
+        
+}
+  
+     var chambre = new Chambre();
+  var observable = new ObservableChambre(chambre);
+  var thermo = new observerThermo();
+  var info = new observerInfo();
+  observable.addObserver(thermo);
+  observable.addObserver(info);
   
        function recursion() {
-        console.log(var1);
-        document.getElementById("test").innerHTML = var1;
-        var1++;
-        setTimeout( recursion, 2000);
+        //console.log(var1);
+        //document.getElementById("test").innerHTML = var1;
+        //var1++;
+        
+        observable.notifyAll();
+           
+        setTimeout( recursion, 50);
     }
 
 /*Code jquery qui affiche un glisseur dans le conteneur ayant
@@ -19,25 +94,23 @@
  */
 /*********************Ne pas modifier***********************/
  $(document).ready(function() {
-     
-   /*  
-  $("#thermostat").slider(
+    
+  $("#thermostatSlider").slider(
   {
     orientation: 'vertical',
     max: 40 ,
-    value:temperatureThermostat,
+    value:0,
     min: -10 ,
     step: 1
   });
-  $("#thermostat").slider({
+  $("#thermostatSlider").slider({
     change: function(event, ui) {
       $("#tdValeurThermostat").text( ui.value );
     }
   });
- */
   
-
- 
+  
+   
   recursion(var1);
   
   /*setInterval(function(){
